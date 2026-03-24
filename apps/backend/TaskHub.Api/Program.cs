da2690bcde2935cd4739032836b8ddf05978cc39
Mikhail Karasev <mikhalkarasev.mkm@gmail.com>
2026-03-25 01:24:57 +0700

    feat(backend): add Swagger support

--- apps/backend/TaskHub.Api/Program.cs
+++ apps/backend/TaskHub.Api/Program.cs
@@ -4,12 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Для работы со swagger --
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//--
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    //Для работы со swagger --
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //--
 }
 
 app.UseHttpsRedirection();
