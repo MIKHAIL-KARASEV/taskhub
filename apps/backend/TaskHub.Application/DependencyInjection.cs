79295bacada8d178293f4a19352dfb6773c33e60
Mikhail Karasev <mikhalkarasev.mkm@gmail.com>
2026-03-29 02:03:16 +0700

    feat(backend-application): register user task services in DI

--- apps/backend/TaskHub.Application/DependencyInjection.cs
+++ apps/backend/TaskHub.Application/DependencyInjection.cs
@@ -2,6 +2,12 @@ namespace TaskHub.Application;
 
 using TaskHub.Application.Users.Login;
 using TaskHub.Application.Users.Register;
+using TaskHub.Application.UserTasks.Create;
+using TaskHub.Application.UserTasks.Complete;
+using TaskHub.Application.UserTasks.Uncomplete;
+using TaskHub.Application.UserTasks.Delete;
+using TaskHub.Application.UserTasks.Update;
+using TaskHub.Application.UserTasks.Queries.GetMyTasks;
 
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,6 +17,12 @@ public static class DependencyInjection
     {
         services.AddScoped<RegisterUserHandler>();
         services.AddScoped<LoginUserHandler>();
+        services.AddScoped<CreateUserTaskHandler>();
+        services.AddScoped<UpdateUserTaskHandler>();
+        services.AddScoped<CompleteUserTaskHandler>();
+        services.AddScoped<UncompleteUserTaskHandler>();
+        services.AddScoped<DeleteUserTaskHandler>();
+        services.AddScoped<GetMyTasksHandler>();
         // позже добавим:
         // - MediatR
         // - Validators
