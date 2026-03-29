fefe15198f54d722ae2997dee20b2e78700bb502
Mikhail Karasev <mikhalkarasev.mkm@gmail.com>
2026-03-29 16:26:42 +0700

    feat(frontend-api): add base request and userTask api

---
+++ apps/frontend/taskhub-frontend/src/entities/userTask/api/userTask.api.ts
@@ -0,0 +1,38 @@
+import { request } from "@/shared/api/base";
+import { UserTask } from "../model/userTask.types";
+
+export const getMyTasks = () =>
+    request<UserTask[]>("/api/userTasks");
+
+export const createTask = (data: {
+    title: string;
+    description?: string;
+}) =>
+    request<UserTask>("Generate request in HTTP Client/tasks", {
+        method: "POST",
+        body: JSON.stringify(data),
+    });
+
+export const updateTask = (id: string, data: {
+    title: string;
+    description?: string;
+}) =>
+    request(`/api/userTasks/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+    });
+
+export const deleteTask = (id: string) =>
+    request(`/api/userTasks/${id}`, {
+        method: "DELETE",
+    });
+
+export const completeTask = (id: string) =>
+    request(`/api/userTasks/${id}/complete`, {
+        method: "POST",
+    });
+
+export const uncompleteTask = (id: string) =>
+    request(`/api/userTasks/${id}/uncomplete`, {
+        method: "POST",
+    });
--- apps/frontend/taskhub-frontend/src/shared/api/base.ts
+++ apps/frontend/taskhub-frontend/src/shared/api/base.ts
@@ -1,1 +1,31 @@
-export const API_URL = 'http://localhost:3000/api';
+import { useAuthStore } from "../lib/auth.store";
+
+export const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+
+export async function request<T>(
+    url: string,
+    options?: RequestInit
+): Promise<T> {
+    const token = useAuthStore.getState().token;
+
+    const res = await fetch(`${API_URL}${url}`, {
+        ...options,
+        headers: {
+            "Content-Type": "application/json",
+            ...(token ? { Authorization: `Bearer ${token}` } : {}),
+            ...(options?.headers || {}),
+        },
+    });
+
+    if (!res.ok) {
+        if (res.status === 401) {
+            useAuthStore.getState().logout();
+        }
+
+        throw new Error("API error");
+    }
+
+    const text = await res.text();
+
+    return text ? JSON.parse(text) : ({} as T);
+}
