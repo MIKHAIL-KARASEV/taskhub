fbf0ebf4065a614072ed8568e9264cc7c2836532
Mikhail Karasev <mikhalkarasev.mkm@gmail.com>
2026-03-29 20:22:16 +0700

    refactor(frontend-userTask): remove deprecated complete logic

--- apps/frontend/taskhub-frontend/src/features/userTask/complete-userTask/model/useCompleteUserTask.ts
+++
@@ -1,9 +0,0 @@
-import { completeTask } from "@/entities/userTask/api/userTask.api";
-
-export function useCompleteUserTask() {
-    const mutate = async (id: string) => {
-        return completeTask(id);
-    };
-
-    return { mutate };
-}
