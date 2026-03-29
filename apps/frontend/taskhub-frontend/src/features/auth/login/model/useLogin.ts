1287a5af3051b9365f2080196fc687787f08f474
MIKHAIL-KARASEV <mikhailkarasev.mkm@gmail.com>
2026-03-29 17:37:22 +0700

    Update useLogin.ts

--- apps/frontend/taskhub-frontend/src/features/auth/login/model/useLogin.ts
+++ apps/frontend/taskhub-frontend/src/features/auth/login/model/useLogin.ts
@@ -1,22 +1,29 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/shared/lib/auth.store";
 import { login } from "@/entities/user/api/auth.api";
-import { useRouter } from "next/navigation";
 
 export function useLogin() {
     const setToken = useAuthStore((s) => s.setToken);
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
     const mutate = async (email: string, password: string) => {
-        const res = await login({ email, password });
+        setLoading(true);
 
-        setToken(res.token);
+        try {
+            const res = await login({ email, password });
 
-        router.push("/userTasks");
+            setToken(res.token);
+            router.push("/userTasks");
 
-        return res;
+            return res;
+        } finally {
+            setLoading(false);
+        }
     };
 
-    return { mutate };
+    return { mutate, loading };
 }
