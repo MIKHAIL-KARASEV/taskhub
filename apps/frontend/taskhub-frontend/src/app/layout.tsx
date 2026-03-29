18ca54aaabc6e63be137811f7bdd31f50f5e4269
Mikhail Karasev <mikhalkarasev.mkm@gmail.com>
2026-03-29 20:23:33 +0700

    chore(frontend-ui): update global styles and layout

--- apps/frontend/taskhub-frontend/src/app/layout.tsx
+++ apps/frontend/taskhub-frontend/src/app/layout.tsx
@@ -1,9 +1,13 @@
 import './styles/globals.css';
+import {Header} from "@/widgets/header/ui/Header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
